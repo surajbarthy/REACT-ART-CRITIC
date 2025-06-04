67422cbd327a8c40739db7e0ae4b4b3c0a4abebf
Suraj Barthy <surajbarthydesign@gmail.com>
2025-06-04 16:17:53 +0530

    Updates

---
+++ precache-manifest.aac44e0a79d6ccfa15c5bd7ff08fd995.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "09ada3a1ba331555977d263fddc10c3b",
+    "url": "/REACT-ART-CRITIC/index.html"
+  },
+  {
+    "revision": "dd13e1fede0a3520c246",
+    "url": "/REACT-ART-CRITIC/static/css/main.1624d5f1.chunk.css"
+  },
+  {
+    "revision": "823bf2ada3b7f6c2aa88",
+    "url": "/REACT-ART-CRITIC/static/js/2.6f2b4fb8.chunk.js"
+  },
+  {
+    "revision": "fc0ba07c0f771d66c44444381376eaf8",
+    "url": "/REACT-ART-CRITIC/static/js/2.6f2b4fb8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "75f6b689b3435993093a",
+    "url": "/REACT-ART-CRITIC/static/js/3.331ae4c8.chunk.js"
+  },
+  {
+    "revision": "dd13e1fede0a3520c246",
+    "url": "/REACT-ART-CRITIC/static/js/main.9c642fd7.chunk.js"
+  },
+  {
+    "revision": "dc223ebe1f2283b5e676",
+    "url": "/REACT-ART-CRITIC/static/js/runtime-main.1240eec6.js"
+  }
+]);
